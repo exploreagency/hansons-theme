3d0a4c14ce69030d330564a502c483f1b60e20c1
orestnowosad <nowosad.orest@gmail.com>
2026-06-01 14:26:23 -0400

    Add lp-footer block layout + styles

--- blocks/lp-footer/block-render.php
+++ blocks/lp-footer/block-render.php
@@ -8,18 +8,45 @@ $copyright   = get_field( 'copyright' );
 $legal_links = get_field( 'legal_links' );
 ?>
 
-<footer <?php echo vt_block_attributes( 'lp-footer', $block ); ?>>
-  <?php if ( $sticky_cta ) :
-    
-    ?>
-    <a href=""
-       class="lp-footer__sticky-cta"
+<?php if ( $sticky_cta ) :
+  $url = $cta['url'];
+  $label = $cta['title'];
+  ?>
+  <div class="sticky-cta__wrapper">
+    <a href="<?= esc_url( $url ); ?>"
+      class="sticky-cta__link"
       >
-
+      <?= esc_html( $label ); ?>
     </a> 
-  <?php endif; ?>
+  </div>
+<?php endif; ?>
 
+<footer <?php echo vt_block_attributes( 'lp-footer', $block ); ?>>
   <div class="container">
+    <div class="section__wrapper">
+      <?php if ( $copyright ) : ?>
+        <p class="lp-footer__copyright">
+          <?= $copyright; ?>
+        </p>
+      <?php endif; ?>
 
+      <?php if ( $legal_links ) : ?>
+        <div class="lp-footer__legal-links">
+          <?php foreach ( $legal_links as $legal_link ) :
+            $url = $legal_link['url'];
+            $label = $legal_link['title'];
+            ?>
+            <a href="<?= esc_url( $url ); ?>"
+               target="_blank"
+               rel="noopener noreferrer"
+               aria-label="<?= esc_html( $label ); ?>"
+               class="lp-footer__legal-link"
+              >
+              <?= esc_html( $label ); ?>
+            </a>
+          <?php endforeach; ?>
+        </div>
+      <?php endif; ?>
+    </div>
   </div>
 </footer>
