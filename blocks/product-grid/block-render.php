66d24fa229857f925cf96da4f35db31b15298e18
orestnowosad <nowosad.orest@gmail.com>
2026-06-02 17:22:12 -0400

    Add product-grid block layout + styles

--- blocks/product-grid/block-render.php
+++ blocks/product-grid/block-render.php
@@ -9,5 +9,61 @@ $products_list    = get_field( 'products_list' );
 ?>
 
 <section <?php echo vt_block_attributes( 'product-grid', $block ); ?>>
+  <div class="section">
+    <div class="container">
+      <div class="section__wrapper">
+        <div class="section__heading section__heading--center"
+             data-orientation="column"
+          >
+          <?php if ( $heading ) : ?>
+            <h2 class="product-grid__heading">
+              <?= wp_kses_post( $heading ); ?>
+            </h2>
+          <?php endif; ?>
 
+          <?php if ( $description ) : ?>
+            <div class="product-grid__description">
+              <?= wp_kses_post( $description ); ?>
+            </div>
+          <?php endif; ?>
+        </div>
+
+        <?php if ( $products_list ) : ?>
+          <div class="product-grid__products">
+            <?php foreach ( $products_list as $product ) :
+              $best_check = $product['best_check'];
+              $heading = $product['heading'];
+              $image = $product['image'];
+              $subheading = $product['subheading'];
+              $feature_list = $product['feature_list'];
+              ?>
+              <div class="product-grid__product">
+                <h3 class="product-grid__product__heading">
+                  <?= wp_kses_post( $heading ); ?>
+                </h3>
+                <div class="product-grid__product__divider"></div>
+                <img src="<?= esc_url( wp_get_attachment_image_src( $image, 'full' )[0] ); ?>"
+                     alt="<?= esc_attr( get_post_meta( $image, '_wp_attachment_image_alt', TRUE ) ); ?>"
+                     loading="lazy"
+                     class="product-grid__product__image"
+                  />
+                <h4 class="product-grid__product__subheading">
+                  <?= wp_kses_post( $subheading ); ?>
+                </h4>
+                <div class="product-grid__product__feature-list">
+                  <?php foreach ( $feature_list as $feature ) :
+                    $item = $feature['feature'];
+                    ?>
+                    <p class="product-grid__product__feature">
+                      <?= wp_kses_post( $item ); ?>
+                    </p>
+                  <?php endforeach; ?>
+                </div>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
 </section>
--- blocks/product-table/block-render.php
+++ blocks/product-table/block-render.php
@@ -3,13 +3,30 @@
  * Product Table Block
  */
 
-$heading          = get_field( 'heading' );
-$offer            = get_field( 'offer' );
-$offer_disclosure = get_field( 'offer_disclosure' );
-$form_shortcode   = get_field( 'form_shortcode' );
-$image            = get_field( 'image' );
+$heading       = get_field( 'heading' );
+$products_list = get_field( 'offer_disclosure' );
 ?>
 
 <section <?php echo vt_block_attributes( 'product-table', $block ); ?>>
+  <div class="section">
+    <div class="container">
+      <div class="section__wrapper">
+        <div class="section__heading section__heading--center"
+             data-orientation="column"
+          >
+          <?php if ( $heading ) : ?>
+            <h2 class="product-table__heading">
+              <?= wp_kses_post( $heading ); ?>
+            </h2>
+          <?php endif; ?>
+        </div>
 
+        <?php if ( $products_list ) : ?>
+          <div class="product-table__products">
+
+          </div>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
 </section>
