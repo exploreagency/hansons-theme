7a4ddbae1a30710d7eda71ccb66976d4ba18319b
orestnowosad <nowosad.orest@gmail.com>
2026-06-02 08:27:51 -0400

    Add certifications block layout + styles

--- blocks/certifications/block-render.php
+++ blocks/certifications/block-render.php
@@ -8,5 +8,29 @@ $certifications_gallery = get_field( 'description' );
 ?>
 
 <section <?php echo vt_block_attributes( 'certifications', $block ); ?>>
+  <div class="container">
+    <div class="certifications__wrapper">
+      <div class="section__wrapper">
+        <?php if ( $heading ) : ?>
+          <h2 class="certifications__heading">
+            <?= wp_kses_post( $heading ); ?>
+          </h2>
+        <?php endif; ?>
 
+        <?php if ( $certifications_gallery ) : ?>
+          <div class="certifications__gallery">
+            <?php foreach ( $certifications_gallery as $certification ) :
+              $image = $certification['image'];
+              ?>
+              <img src="<?= esc_url( wp_get_attachment_image_src( $image, 'full' )[0] ); ?>"
+                   alt="<?= esc_attr( get_post_meta( $image, '_wp_attachment_image_alt', TRUE ) ); ?>"
+                   loading="lazy"
+                   class="certifications__image"
+                />
+            <?php endforeach; ?>
+          </div>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
 </section>
