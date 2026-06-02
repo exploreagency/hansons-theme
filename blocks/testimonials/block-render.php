f2d156cd658c4b9cecd63f09ca61fa2a72617788
orestnowosad <nowosad.orest@gmail.com>
2026-06-02 11:55:16 -0400

    Add testimonials block layout + styles

--- blocks/testimonials/block-render.php
+++ blocks/testimonials/block-render.php
@@ -9,5 +9,77 @@ $testimonials_list = get_field( 'testimonials_list' );
 ?>
 
 <section <?php echo vt_block_attributes( 'testimonials', $block ); ?>>
+  <div class="container">
+    <div class="section__wrapper">
+      <div class="section__heading section__heading--center">
+        <?php if ( $heading ) : ?>
+          <h2 class="testimonials__heading">
+            <?= wp_kses_post( $heading ); ?>
+          </h2>
+        <?php endif; ?>
 
+        <?php if ( $rating ) : ?>
+          <p class="testimonials__rating">
+            <span></span>
+          </p>
+        <?php endif; ?>
+      </div>
+
+      <?php if ( $testimonials_list ) : ?>
+        <div class="testimonials__carousel"
+             data-embla
+             data-options="{ 'loop': true }"
+          >
+          <div class="embla__viewport">
+            <div class="embla__wrapper">
+              <?php foreach ( $testimonials_list as $testimonial ) :
+                $name = $testimonial['name'];
+                $review = $testimonial['review'];
+                $source = $testimonial['source'];
+                ?>
+                <div class="testimonials__carousel__slide">
+                  <div class="testimonials__carousel__slide__container">
+                    <div class="testimonials__carousel__slide__wrapper">
+                      <div class="testimonials__carousel__slide__content">
+                        <div class="testimonials__carousel__slide__heading">
+                          <svg width="23" height="14" viewBox="0 0 23 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2.52239 0.5H11.0112L5.57836 13.5H0L2.52239 0.5ZM13.5336 0.5H22.0224L16.5896 13.5H11.0112L13.5336 0.5Z" fill="currentColor"/>
+                          </svg>
+                          <span class="testimonials__carousel__slide__rating">
+                            ★★★★★
+                          </span>
+                        </div>
+
+                        <p class="testimonials__carousel__slide__review">
+                          <?= wp_kses_post( $review ); ?>
+                        </p>
+                      </div>
+
+                      <div class="testimonials__carousel__slide__footing">
+                        <div class="testimonials__carousel__slide__divider"></div>
+
+                        <div class="testimonials__carousel__slide__source">
+                          <p class="testimonials__carousel__slide__name">
+                            <?= wp_kses_post( $name ); ?>
+                          </p>
+
+                          <img src="<?= esc_url( wp_get_attachment_image_src( $source, 'testimonial-source' )[0] ); ?>"
+                               alt="<?= esc_attr( get_post_meta( $source, '_wp_attachment_image_alt', TRUE ) ); ?>"
+                               loading="lazy"
+                               class="testimonials__carousel__slide__icon"
+                            />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              <?php endforeach; ?>
+            </div>
+          </div>
+
+
+        </div>
+      <?php endif; ?>
+    </div>
+  </div>
 </section>
